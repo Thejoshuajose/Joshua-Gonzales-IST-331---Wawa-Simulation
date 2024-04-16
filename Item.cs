658da70f17368f909f76e5a6be851e4495871790
Thejoshuajose <joshuajose3@icloud.com>
2024-04-16 16:12:35 -0400

    First Commit (String reader and class created as well as some design implimentations mainly the reciept)

---
+++ Item.cs
@@ -0,0 +1,106 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Dynamic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Joshua_Gonzales___IST_331___Wawa_Simulation
+{
+    class Item
+    {
+        string itemName;
+        double itemPrice;
+        string itemDescription;
+        string itemType;
+        string itemImgPath;
+        public List<string> itemCart = new List<string>();
+        public List<double> itemPriceCart = new List<double>();
+
+
+        public Item(string itemName, string itemDescription, string itemType, double itemPrice, string itemImgPath)
+        {
+            this.itemName = itemName;
+            this.itemPrice = itemPrice;
+            this.itemDescription = itemDescription;
+            this.itemType = itemType;
+            this.itemImgPath = itemImgPath;
+        }
+
+        public Item(string itemName, double itemPrice, string itemType,string itemImgPath)
+        {
+            this.itemName = itemName;
+            this.itemPrice = itemPrice;
+            this.itemType = itemType;
+            this.itemImgPath = itemImgPath;
+        }
+
+        public List<string> GetItemCart()
+        {
+            return this.itemCart;
+        }
+
+        public string GetItemName()
+        {
+            return itemName;
+        }
+
+        public double GetItemPrice()
+        {
+            return itemPrice;
+        }
+
+        public string GetItemDescription()
+        {
+            return itemDescription;
+        }
+
+        public string GetItemType()
+        {
+            return itemType;
+        }
+        public string GetItemImgPath()
+        {
+            return itemImgPath;
+        }
+
+        public void AddToCart (string itemName, double itemPrice)
+        {
+            itemCart.Add(itemName);
+            itemPriceCart.Add(itemPrice);
+
+        }
+
+        public void RemoveFromCart(string itemName, double itemPrice) 
+        { 
+            itemCart.Remove(itemName); 
+            itemPriceCart.Remove(itemPrice);
+        }
+        public void SetItemImgPath(string imgPath)
+        {
+            this.itemImgPath = imgPath;
+        }
+
+        public void SetItemName(string name)
+        {
+            itemName = name;
+        }
+
+        public void SetItemPrice(double price)
+        {
+            itemPrice = price;
+        }
+
+        public void SetItemDescription(string description)
+        {
+            itemDescription = description;
+        }
+
+        public void SetItemType(string type)
+        {
+            itemType = type;
+        }
+
+    }
+}
--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System.Text;
+﻿using System.IO;
+using System.Text;
 using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Data;
@@ -14,11 +15,55 @@ namespace Joshua_Gonzales___IST_331___Wawa_Simulation
     /// <summary>
     /// Interaction logic for MainWindow.xaml
     /// </summary>
+    ///
     public partial class MainWindow : Window
     {
+
+        public List<string> itemNames = new List<string>();
+        public List<string> itemTypes = new List<string>();
+        public List<string> itemDescripts = new List<string>();
+        public List<double> itemPrice = new List<double>();
+        public List<string> itemPath = new List<string>();
+        public List<string> items = new List<string>();
+
         public MainWindow()
         {
             InitializeComponent();
+            InitalizeItems();
+        }
+
+
+        private void InitalizeItems()
+        {
+
+            String line;
+            try
+            {
+                StreamReader reader = new StreamReader("ItemInformation.txt");
+                line = reader.ReadLine();
+                while (line != null)
+                {
+                    items.Add(line);
+                    itemNames.Add(line.Split('~')[0].Trim());
+                    itemTypes.Add(line.Split('~')[1].Trim());
+                    itemDescripts.Add(line.Split('~')[2].Trim());
+                    itemPrice.Add(double.Parse(line.Split('~')[3].Trim()));
+                    itemPath.Add(line.Split('~')[4].Trim());
+
+
+
+                    line = reader.ReadLine();
+                }
+                reader.Close();
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("Exception: " + e.Message);
+            }
+            finally
+            {
+                Console.WriteLine("Executing final Stock.");
+            }
         }
     }
 }
