abb6ffdd490e56e309b2b6c437f06c12ddd904fd
Thejoshuajose <joshuajose3@icloud.com>
2024-04-16 15:14:25 -0400

    Add project files.

---
+++ App.xaml.cs
@@ -0,0 +1,14 @@
+﻿using System.Configuration;
+using System.Data;
+using System.Windows;
+
+namespace Joshua_Gonzales___IST_331___Wawa_Simulation
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+    }
+
+}
