d9556b19397080f63fe8503c214d49f2212e0649
Thejoshuajose <joshuajose3@icloud.com>
2024-04-16 21:19:32 -0400

    working on Text

---
+++ Hoagies.xaml.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace Joshua_Gonzales___IST_331___Wawa_Simulation
+{
+    /// <summary>
+    /// Interaction logic for Hoagies.xaml
+    /// </summary>
+    public partial class Hoagies : Window
+    {
+        public Hoagies()
+        {
+            InitializeComponent();
+        }
+    }
+}
